d6e19c41bc0a28a771dc69c923700a348cfb1723
Yongjian <Huangyj2002@gmail.com>
2023-05-29 17:55:01 +0800

    Created backend v1

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,44 @@
-const {SgidClient} = require('@opengovsg/sgid-client')
-const cors = require("cors")
-// const cookieParser = require('cookie-parser')
-//const open =  require('open')
-const apiRouter = require("./controller/auth")
 require('dotenv').config()
 const express = require('express')
+const cors = require("cors")
+const mongoose = require('mongoose')
+
+const apiRouter = require("./controller/route")
+const Data = require("./models/data")
+//const RecyclePoint = require("./models/recyclePoints")
 const app = express() 
+const {data} = require("./data/mockdata")
+
+MONGODB_URI = process.env.MONGODB_URI
+PORT = process.env.PORT
+
+mongoose.set('strictQuery', false)
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+      Data.deleteMany({}).then(function(){
+        console.log("Database reset")  // Success
+    }).catch(function(error){
+        console.log(error)      // Failure
+    })  
+      Data.insertMany(data).then(function(){
+        console.log("Data inserted")  // Success
+    }).catch(function(error){
+        console.log(error)      // Failure
+    })  
+
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
 app.use(cors())
+app.use(express.json())
+app.use("/api/data", apiRouter)
 
-const PORT = 5001
-
-// Initialize the sgID SDK here
-const sgidClient = new SgidClient({
-clientId: String(process.env.SGID_CLIENT_ID),
-clientSecret: String(process.env.SGID_CLIENT_SECRET),
-privateKey: String(process.env.SGID_PRIVATE_KEY),
-redirectUri: String(process.env.SGID_REDIRECT_URI)
-})
-
-
-
-
-const initServer = async () => {
-try {
-    // app.use(cookieParser())
-    app.use('/api', apiRouter)
-
-    app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-    //void open(`http://localhost:${PORT}`)
-    })
-} catch (error) {
-    console.error(
-    'Something went wrong while starting the server. Please restart the server.',
-    )
-    console.error(error)
-}
-}
-
-initServer()
-
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
