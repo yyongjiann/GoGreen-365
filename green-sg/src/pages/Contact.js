0a0a8453cece6232f93af2091943050c05e87e10
Yongjian <Huangyj2002@gmail.com>
2023-05-29 21:13:03 +0800

    Preserving updated backend only

--- green-sg/src/pages/Contact.js
+++
@@ -1,5 +0,0 @@
-const Contact = () => {
-    return <h1>Contact Info</h1>;
-  };
-  
-  export default Contact;
--- green-sg/src/pages/Profile.js
+++
@@ -1,5 +0,0 @@
-const Profile = () => {
-    return <h1>User Profile</h1>;
-  };
-  
-  export default Profile;
